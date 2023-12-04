5f751892038d6925fff3e664db2f217159ee2b48
Siddiksid <siddiksid15@gmail.com>
2023-12-03 20:51:47 -0800

    minor fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -140,6 +140,7 @@ export default function Home() {
             type="button"
             onClick={(e) => (
               (dayBox[selectedDay] = ""),
+              setText(""),
               setIsUpdated(false),
               setTextId("textAble"),
               setDivId("divDisable")
